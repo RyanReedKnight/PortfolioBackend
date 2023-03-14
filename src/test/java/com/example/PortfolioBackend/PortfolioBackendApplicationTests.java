d38225ff7f6a297977faf532f44bc3f171469089
RyanReedKnight <ryanreedknight@gmail.com>
2023-03-13 18:45:06 -0700

    Initial commit, test suite working with its own application.yml file

---
+++ src/test/java/com/example/PortfolioBackend/PortfolioBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.PortfolioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application.yml")
+class PortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
